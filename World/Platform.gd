d3b145257d44872415c49f7351bd4dc3afc387e2
DR3NK0 <thedrenkoshow@hotmail.com>
2024-09-17 12:08:57 +0200

    Clear

--- World/Platform.gd
+++
@@ -1,5 +0,0 @@
-extends MeshInstance3D
-
-
-func _on_totem_has_been_activated():
-	$AnimationPlayer.play("platform")
--- World/end_game.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-func _on_timer_timeout():
-	Global.collected = [0,0,0]
-	Global.collectableTotal = [0,0,0]
-	Global.totemsActivados = 0
-	get_tree().change_scene_to_file("res://World/world.tscn")
